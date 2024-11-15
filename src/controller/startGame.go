5b985eae3138144eace934301b93c5d3f23fd51d
Yoan Commeau <yoan.commeau@ynov.com>
2024-11-15 09:00:09 +0100

    choix de difficulté

--- src/controller/startGame.go
+++ src/controller/startGame.go
@@ -1,31 +1,43 @@
 package controller
 
 import (
-	HangmanWeb "HangmanWeb/src/function"
+	Functions "HangmanWeb/src/function"
 	"fmt"
 	"net/http"
 	"os"
 	"text/template"
 )
+
 type dataPage struct {
-	Word     string
-	TabUnder []string
+	Word              string
+	TabUnder          []string
 	LetterGuessedList []string
-	WordGuessedList []string
-	AttemptsLeft int
-	HangmanImage string
-	IsGameOver bool
-	IsWin bool
+	WordGuessedList   []string
+	AttemptsLeft      int
+	HangmanImage      string
+	IsGameOver        bool
+	IsWin             bool
 }
+
 func StartGame(w http.ResponseWriter, r *http.Request) {
 	temp, tempErr := template.ParseFiles("./src/temps/display.html")
 	if tempErr != nil {
 		fmt.Println("Error parsing templates: display")
 		os.Exit(1)
 	}
-
-	data := HangmanWeb.ShowTextFromFile("src/data/wordList/wordList_Easy.txt")
-
+	Functions.ShowTextFromFile(r.FormValue("difficulty"),)
+	Functions.Underscore(Functions.Word)
+	Functions.NbrRandom()
+	
+	data := dataPage{
+	Word:              Functions.Word,
+    TabUnder:          Functions.TabUnder,
+    LetterGuessedList: Functions.LetterGuessedList,
+    WordGuessedList:   Functions.WordGuessedList,
+    AttemptsLeft:      Functions.AttemptsLeft,
+	HangmanImage:      "",
+    IsGameOver:        false,
+    IsWin:             false,
+    }
 	temp.ExecuteTemplate(w, "display", data)
-
 }
--- src/function/func.go
+++ src/function/func.go
@@ -1,4 +1,4 @@
-package HangmanWeb
+package Functions
 
 import (
 	"bufio"
